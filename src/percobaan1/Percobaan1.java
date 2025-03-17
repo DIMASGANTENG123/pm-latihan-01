c9d65b259924214fbd86e83143b31e18d42e5b83
DIMASGANTENG123 <dimasmochtar0@gmail.com>
2025-03-17 14:45:57 +0700

    pm latihan 01 dimas

---
+++ src/percobaan1/Percobaan1.java
@@ -0,0 +1,22 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package percobaan1;
+
+/**
+ *
+ * @author User
+ */
+public class Percobaan1 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+       Mahasiswa mhs=new Mahasiswa();
+       mhs.menampilkanInfo();
+        
+    }
+    
+}
